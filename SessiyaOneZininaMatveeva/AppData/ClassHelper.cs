1031bf7e32fa4905f48ee55de9bd555e549af46b
User25 <User25@WIN-L7DRVG5CDCM>
2024-10-23 14:40:32 +0300

    Добавьте файлы проекта.

---
+++ SessiyaOneZininaMatveeva/AppData/ClassHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SessiyaOneZininaMatveeva.AppData
+{
+    internal class ClassHelper
+    {
+        public static bool Authorise(string login, string password, string role)
+        {
+            if (login == string.Empty || password == string.Empty) 
+            {
+                MessageBoxHelper.Error("Заполните все поля!");
+                return false;
+            }
+            else
+            {
+                if (role == "Организатор")
+                {
+                    List<Organizer> organizers
+                }
+            }
+        }
+    }
+}
