23da561f4a2273b351f1de6b01913f9efb8016c5
User25 <User25@WIN-L7DRVG5CDCM>
2024-10-24 13:36:17 +0300

    Написаны все окна.

--- SessiyaOneZininaMatveeva/View/Windows/EventsWindow.xaml.cs
+++ SessiyaOneZininaMatveeva/View/Windows/EventsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SessiyaOneZininaMatveeva.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,24 +20,45 @@ namespace SessiyaOneZininaMatveeva.View.Windows
     /// </summary>
     public partial class EventsWindow : Window
     {
+        private static user25Entities1 context = App.GetContext();
+        List<Direction> directions = context.Direction.ToList();
+        List<Event> events = context.Event.ToList();
+
         public EventsWindow()
         {
             InitializeComponent();
+            EventsLb.ItemsSource = context.Event.ToList();
+            directions.Insert(0, new Direction() { Name = "Все направления" });
+            DirectionCmb.ItemsSource = directions;
+            DirectionCmb.DisplayMemberPath = "Name";
         }
 
         private void SignInHL_Click(object sender, RoutedEventArgs e)
         {
-
+            StartWindow startWindow = new StartWindow();
+            startWindow.Show();
+            Close();
         }
 
         private void DirectionCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            events = context.Event.ToList();
+            if (DirectionCmb.SelectedIndex == 0)
+            {
+                events = context.Event.ToList();
+                EventsLb.ItemsSource = events;
+            }
+            else
+            {
+                events = events.Where(ev => ev.Direction == DirectionCmb.SelectedItem as Direction).ToList();
+                EventsLb.ItemsSource = events;
+            }
         }
 
         private void EventsLb_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-
+            EventInfoWindow eventInfoWIndow = new EventInfoWindow(EventsLb.SelectedItem as Event);
+            eventInfoWIndow.ShowDialog();
         }
     }
 }
