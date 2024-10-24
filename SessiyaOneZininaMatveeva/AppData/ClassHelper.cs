069848c8b0a1cc8a0cfcae0bef5db65a62ec2a1a
User25 <User25@WIN-L7DRVG5CDCM>
2024-10-24 11:20:36 +0300

    стили готовы

--- SessiyaOneZininaMatveeva/AppData/ClassHelper.cs
+++ SessiyaOneZininaMatveeva/AppData/ClassHelper.cs
@@ -9,20 +9,6 @@ namespace SessiyaOneZininaMatveeva.AppData
 {
     internal class ClassHelper
     {
-        //public static bool Authorise(string login, string password, string role)
-        //{
-        //    if (login == string.Empty || password == string.Empty) 
-        //    {
-        //        MessageBoxHelper.Error("Заполните все поля!");
-        //        return false;
-        //    }
-        //    else
-        //    {
-        //        if (role == "Организатор")
-        //        {
-        //            List<Organizer> organizers
-        //        }
-        //    }
-        //}
+        
     } 
 }
