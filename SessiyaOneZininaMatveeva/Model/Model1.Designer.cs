9cb75ca453f1adb3ba36595556b8893d29778aff
User25 <User25@WIN-L7DRVG5CDCM>
2024-10-25 11:35:00 +0300

    снова обновлена модель

--- SessiyaOneZininaMatveeva/Model/Model1.Designer.cs
+++ SessiyaOneZininaMatveeva/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Adm\source\repos\SessiyaOneZininaMatveeva\SessiyaOneZininaMatveeva\Model\BdModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Adm\source\repos\SessiyaOneZininaMatveeva\SessiyaOneZininaMatveeva\Model\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
